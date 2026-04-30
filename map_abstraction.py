df79a584b22aa68f6e65839e81bd7555a53a48d6
charl024 <comaoeng2003@gmail.com>
2026-04-30 13:48:16 -0600

    added map abstraction stuff, added bmps

--- map_abstraction.py
+++ map_abstraction.py
@@ -1,5 +1,41 @@
 # contains map abstraction functions
 
-def convert_bmp_to_map(map_bmp):
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def load_png_to_map(png_path):
+
     pass
     # return map_abstraction (guessing it's a 2d matrix, subsampled accordingly)
+
+def upsample(map):
+    pass
+
+def downsample(map):
+    pass
+
+def load_bmp_to_map(bmp_path, map_dim_width=40, map_dim_height=40):
+    bmp_img = Image.open(bmp_path)
+    bmp_arr = np.array(bmp_img)
+    img_width, img_height = bmp_arr.shape
+
+    if (img_width > map_dim_width and img_height > map_dim_height):
+        resulting_map = upsample(bmp_arr)
+    elif (img_width < map_dim_width and img_height < map_dim_height):
+        resulting_map = downsample(bmp_arr)
+
+    print(bmp_arr)
+
+    plt.imshow(bmp_arr)
+    plt.show()
+
+    print(bmp_arr.shape)
+
+    pass
+    # return map_abstraction (guessing it's a 2d matrix, subsampled accordingly)
+
+load_bmp_to_map("./map_bmps/map1.bmp")
+load_bmp_to_map("./map_bmps/map2.bmp")
+load_bmp_to_map("./map_bmps/map3.bmp")
+load_bmp_to_map("./map_bmps/map4.bmp")
