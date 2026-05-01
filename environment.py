a778c35a096e01a50fc8c45a4212341df29f7ae7
charl024 <comaoeng2003@gmail.com>
2026-05-01 16:41:20 -0600

    began filling out environment class, need to fill in update function

--- environment.py
+++ environment.py
@@ -1,20 +1,50 @@
 # Environment class
 
+import matplotlib.pyplot as plt
+
 class Environment():
+    # map_abstraction - 2d array
+    # target position - position in map, tuple
     def __init__(self, map_abstraction, target_position):
-        self.map_abstraction = map_abstraction
+        self.map = map_abstraction
         self.target_position = target_position
 
         # list of actions an agent can perform in the environment
-        self.actions = null
+        self.actions = [
+            # (x, y)
+            (0, 1),  # North, 0, Up
+            (1, 0),  # East,  1, Right
+            (0, -1), # South, 2, Down
+            (-1, 0), # West,  3, Left
+        ]
     
     # state - position in map
-    # action - some way to choose a specific action from actions (idx into an array? idk)
+    # action - some way to choose a specific action from actions (integer value, idx)
     # reward_strategy - some reward function we can define 
     def update(self, state, action, reward_strategy):
-        pass
-        # return next_state, immediate_reward
 
-    # would be helpful to plot it later for testing, adding this for now
-    def visualize():
-        pass
+        # define new state w action
+        dx, dy = self.actions[action]
+        nx = state[0] + dx
+        ny = state[1] + dy
+
+        # check boundary conditiond
+
+        # check obstacle
+
+        # check if target reached
+
+        # otherwise, just move accordingly
+
+    def plot(self):
+        plt.imshow(self.map, cmap="gray_r")
+
+        target_x, target_y = self.target_position
+        plt.scatter(target_x, target_y, c="red", label="target")
+
+        plt.title("Environment Map")
+        plt.show()
+
+# import map_abstraction
+# env = Environment(map_abstraction=map_abstraction.load_bmp_to_map("./map_bmps/map1.bmp"), target_position=(25,5))
+# env.plot()
