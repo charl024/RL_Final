792f7dfd80b990d93f07121c3d88873d900561c6
charl024 <comaoeng2003@gmail.com>
2026-05-01 16:00:37 -0600

    inverted downsampling to do min pooling

--- map_abstraction.py
+++ map_abstraction.py
@@ -28,19 +28,18 @@ def downsample(map_in, map_dim_width=60, map_dim_height=40):
 
     for y in range(map_dim_height):
         for x in range(map_dim_width):
-            max_val = -1
+            min_val = 255
             for patch_y in range(patch_height):
                 for patch_x in range(patch_width):
-                    val = map_in[y*patch_height + patch_y][x * patch_width + patch_x]
-                    max_val = max(max_val, val)
-                    
-            map_out[y][x] = max_val
-    
+                    val = map_in[y * patch_height + patch_y][x * patch_width + patch_x]
+                    min_val = min(min_val, val)
+            map_out[y][x] = min_val
+
     return map_out
 
 
 def load_bmp_to_map(bmp_path, map_dim_width=40, map_dim_height=40):
-    bmp_img = Image.open(bmp_path)
+    bmp_img = Image.open(bmp_path).convert("L")
     bmp_arr = np.array(bmp_img)
     img_height, img_width = bmp_arr.shape
 
@@ -48,17 +47,19 @@ def load_bmp_to_map(bmp_path, map_dim_width=40, map_dim_height=40):
         resulting_map = downsample(bmp_arr, map_dim_width, map_dim_height)
     elif (img_width < map_dim_width and img_height < map_dim_height):
         resulting_map = upsample(bmp_arr, map_dim_width, map_dim_height)
+    else:
+        resulting_map = bmp_arr.astype(float)
 
     # print(bmp_arr)
 
-    # plt.imshow(resulting_map)
-    # plt.show()
+    plt.imshow(resulting_map)
+    plt.show()
 
     print(resulting_map.shape)
 
     return resulting_map
 
 # load_bmp_to_map("./map_bmps/map1.bmp")
-# load_bmp_to_map("./map_bmps/map2.bmp")
-# load_bmp_to_map("./map_bmps/map3.bmp")
-# load_bmp_to_map("./map_bmps/map4.bmp")
+load_bmp_to_map("./map_bmps/map2.bmp")
+load_bmp_to_map("./map_bmps/map3.bmp")
+load_bmp_to_map("./map_bmps/map4.bmp")
