34bab45b033e6cb7f7d18d49d216115d8e587790
charl024 <comaoeng2003@gmail.com>
2026-05-01 16:34:43 -0600

    added some init params for agent, namely for qtable

--- agent.py
+++ agent.py
@@ -3,12 +3,15 @@
 import numpy as np
 
 class Agent():
-    def __init__(self):
+    def __init__(self, width=40, height=40, n_actions=4):
         # q_table is a 3d matrix
-        # self.q_table =
+        self.q_table = np.zeros((height, width, n_actions))
+        self.n_actions = n_actions
         pass 
         
     # state - position in map, used to index into q_table
     # this may be where we use epsilon to choose exploration vs exploitation
     def choose_action(self, state):
+        # explore
+        # else exploit
         pass
