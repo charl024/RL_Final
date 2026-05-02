595ccfa30e20c58a1baa25a2540f9500d7346c84
Joel Villarreal II <joel.villa2329@gmail.com>
2026-05-02 11:29:28 -0600

    moved reward_strategy to constructor, felt natural

--- environment.py
+++ environment.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 class Environment():
     # map_abstraction - 2d array
     # target position - position in map, tuple
-    def __init__(self, map_abstraction, target_position):
+    # reward_strategy - some reward function
+    def __init__(self, map_abstraction, target_position, reward_strategy):
         self.map = map_abstraction
         self.target_position = target_position
+        self.reward_strategy = reward_strategy
 
         # list of actions an agent can perform in the environment
         self.actions = [
@@ -33,7 +35,7 @@ class Environment():
     # state - position in map
     # action - some way to choose a specific action from actions (integer value, idx)
     # reward_strategy - some reward function we can define 
-    def update(self, state, action, reward_strategy):
+    def update(self, state, action):
 
         # define new state w action
         dx, dy = self.actions[action]
@@ -42,15 +44,15 @@ class Environment():
         
         if (nx < 0 or nx >= self.map.shape[1] or ny < 0 or ny >= self.map.shape[0]):
             # out of bounds, return current state and some negative reward
-            reward = reward_strategy("boundary")
+            reward = self.reward_strategy("boundary")
         elif (self.map[ny][nx] == 0):
             # check obstacle: 0 value in map means obstacle
-            reward = reward_strategy("obstacle")
+            reward = self.reward_strategy("obstacle")
         elif (nx, ny) == self.target_position:
             # check target reached
-            reward = reward_strategy("target")
+            reward = self.reward_strategy("target")
         else:
-            reward = reward_strategy("move")
+            reward = self.reward_strategy("move")
         
         return (nx, ny), reward
 
@@ -65,10 +67,10 @@ class Environment():
 
 # import map_abstraction
 # import reward_strategy
-# env = Environment(map_abstraction=map_abstraction.load_bmp_to_map("./map_bmps/map1.bmp"), target_position=(25,5))
+# env = Environment(map_abstraction=map_abstraction.load_bmp_to_map("./map_bmps/map1.bmp"), target_position=(25,5), reward_strategy=reward_strategy.reward_strategy_simple)
 # print(env)
 # env.plot()
-# print(env.update((0,0), 1,reward_strategy.reward_strategy_simple))
-# print(env.update((25,4), 1,reward_strategy.reward_strategy_simple))
-# print(env.update((24,5), 1,reward_strategy.reward_strategy_simple))
-# print(env.update((25,6), 2,reward_strategy.reward_strategy_simple))
+# print(env.update((0,0),  1))
+# print(env.update((25,4), 1))
+# print(env.update((24,5), 1))
+# print(env.update((25,6), 2))
