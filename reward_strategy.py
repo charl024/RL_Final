160bf681e33abf3f12b8b4997551a098ff94ba5a
Joel Villarreal II <joel.villa2329@gmail.com>
2026-05-02 12:25:02 -0600

    new reward strat, simple

--- reward_strategy.py
+++ reward_strategy.py
@@ -6,3 +6,11 @@ def reward_strategy_simple(event):
     if event in ["obstacle", "boundary"]:
         return -100
     return -1
+
+def reward_strategy_distance_based(event):
+    if event == "target":
+        return 100
+    if event in ["obstacle", "boundary"]:
+        return -100
+    # negative reward based on distance to target (manhattan distance? euclidean distance?)
+    return -event
