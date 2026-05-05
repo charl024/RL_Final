823be7baaeb0302dcf5b2788c86cf648008e0876
charl024 <comaoeng2003@gmail.com>
2026-05-04 22:42:05 -0600

    added some more reward_strats

--- reward_strategy.py
+++ reward_strategy.py
@@ -1,25 +1,51 @@
 # define reward strategies here, which return a "reward" value
 
+VISITED_REWARD  = -25
+TARGET_REWARD   = 100
+OBSTACLE_REWARD = -100
+BOUNDARY_REWARD = -100
+DEFAULT_REWARD  = -1
+
+# higher means distance has more weight
+# lower means distance has less weight
+DISTANCE_SCALING_FACTOR = 0.5
+
 def reward_strategy_simple(event):
     if event == "target":
-        return 100
-    if event in ["obstacle", "boundary", "visited"]:
-        return -100
-    return -1
+        return TARGET_REWARD
+    if event in ["obstacle", "boundary"]:
+        return OBSTACLE_REWARD
+    if event == "visited":
+        return VISITED_REWARD
+    return DEFAULT_REWARD
 
 def reward_strategy_distance_based(event):
     if event == "target":
-        return 100
-    if event in ["obstacle", "boundary", "visited"]:
-        return -100
-    # negative reward based on distance to target (manhattan distance? euclidean distance?)
+        return TARGET_REWARD
+    if event in ["obstacle", "boundary"]:
+        return OBSTACLE_REWARD
+    if event == "visited":
+        return VISITED_REWARD
+
+    # manhattan distance
     return -event
 
-def reward_strategy_visited(event):
+def reward_strategy_distance_based_scaled(event):
     if event == "target":
-        return 100
+        return TARGET_REWARD
     if event in ["obstacle", "boundary"]:
-        return -100
+        return OBSTACLE_REWARD
     if event == "visited":
-        return -50
-    return -1
+        return VISITED_REWARD
+
+    # manhattan distance
+    return DEFAULT_REWARD - DISTANCE_SCALING_FACTOR * event
+
+def reward_strategy_harsh_visited(event):
+    if event == "target":
+        return TARGET_REWARD
+    if event in ["obstacle", "boundary"]:
+        return OBSTACLE_REWARD
+    if event == "visited":
+        return -500
+    return DEFAULT_REWARD
