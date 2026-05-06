dd8845673a598d66d269cd45cbeb341c43da14e2
Joel Villarreal II <joel.villa2329@gmail.com>
2026-05-06 10:32:13 -0600

    avg path distance, min, q1, med, q3, and max path distances

--- testing_area.py
+++ testing_area.py
@@ -39,12 +39,90 @@ MAX_STEPS_REWARD_STRAT_SARSA = 200
 # list of reward strategies
 strategies = [reward_strategy_simple, reward_strategy_distance_based, reward_strategy_distance_based_scaled,  reward_strategy_harsh_visited]
 
+def path_stats(agent, environment,  max_path_steps=500):
+    """
+    Test the accuracy of the agent's learned policy by generating a path from 
+    each possible initial position and checking if it reaches the target without 
+    hitting obstacles.
+
+    RETURN - (test accuracy, minimum path distance, Q1, Q2, Q3, maximum path distance) 
+           - test accuracy - percentage of valid paths
+           - minimum path distance - shortest path
+           - Q1 - upper quartile of paths
+           - Q2 - average path length
+           - Q3 - lower quartile of paths
+           - maximum path distance - longest path
+    """
+    valid_paths = 0
+    total_paths = 0
+    path_lengths = np.zeros(environment.height * environment.width)
+
+    for y in range(environment.height):
+        for x in range(environment.width):
+            state = (x, y)
+
+            if state == environment.target_position:
+                continue
+
+            if not environment.in_boundary(state):
+                continue
+
+            if environment.is_obstacle(state):
+                continue
+
+            total_paths += 1
+            path_valid = False
+            path_length = 0
+
+            environment.reset_visited()
+
+            for _ in range(max_path_steps):
+                if state == environment.target_position:
+                    path_valid = True
+                    break
+
+                action = agent.exploit(state)
+                new_state, reward = environment.update(state, action)
+
+                if new_state is None:
+                    break
+
+                # print(new_state)
+                # print(reward)
+
+                if reward <= -100:  # hit obstacle or boundary
+                    #TODO: is this the best way to be doing this? 
+                    path_valid = False
+                    break
+
+                state = new_state
+                path_length += 1
+
+            if path_valid:
+                path_lengths[valid_paths] = path_length
+                valid_paths += 1 
+
+    path_lengths = path_lengths[:valid_paths] # Only valid paths
+
+    test_acc = valid_paths / total_paths
+
+    if (path_lengths.size == 0):
+        # path_lengths emtpy, do not compute min (will throw error)
+        return (test_acc, 0, 0, 0, 0, 0, 0)
+    avg_dist = np.mean(path_lengths)
+    min_dist = min(path_lengths)
+    q1_dist, med_dist, q3_dist = np.percentile(path_lengths, [25, 50, 75])
+    max_dist = max(path_lengths)
+
+    return (test_acc, avg_dist, min_dist, q1_dist, med_dist, q3_dist, max_dist)
+
 
 def test_acc(agent, environment,  max_path_steps=500):
     """
     Test the accuracy of the agent's learned policy by generating a path from 
     each possible initial position and checking if it reaches the target without 
     hitting obstacles.
+    THIS IS NOT USED CURRENTLY (USING path_stats() instead)
 
     RETURN - test accuracy (percentage of valid paths)
     """
@@ -125,9 +203,11 @@ def eval_performance(environment, training_funct, kwargs):
 
     total_time = end_time - start_time
 
-    acc = test_acc(agent, environment)
+    path_stats_tuple = path_stats(agent, environment)
 
-    return total_time, steps, acc
+    # acc = test_acc(agent, environment)
+
+    return (total_time, steps, *path_stats_tuple)
 
 def dummy_training_function(agent,
                             episodes, 
@@ -376,7 +456,7 @@ def get_best_acc(dict):
     best_param_val = 0
 
     for key, value in dict.items():
-        _, _, acc = value
+        acc = value[2]
         if acc > best_acc:
             best_acc = acc
             best_param_val = key
@@ -389,15 +469,19 @@ def print_results(title, sarsa_dict, q_dict):
 
     print("SARSA:")
     for key, value in sarsa_dict.items():
-        time_cost, steps, acc = value
-        print(f"\t\t{key}: ({time_cost:.4f}s, {steps} steps, {acc * 100:.3f}%)")
+        time_cost, steps, acc, avg_dist, min_dist, q1_dist, med_dist, q3_dist, max_dist = value
+        print(f"""\t\t{key}: ({time_cost:.4f}s, {steps} steps, {acc * 100:.3f}%, 
+              \t    avg path distance: {avg_dist:.4f}, 
+              \t    min: {min_dist}, q1: {q1_dist}, median: {med_dist}, q3: {q3_dist}, max: {max_dist})""")
 
     print()
 
     print("Q-Learning:")
     for key, value in q_dict.items():
-        time_cost, steps, acc = value
-        print(f"\t\t{key}: ({time_cost:.4f}s, {steps} steps, {acc * 100:.3f}%)")
+        time_cost, steps, acc, avg_dist, min_dist, q1_dist, med_dist, q3_dist, max_dist = value
+        print(f"""\t\t{key}: ({time_cost:.4f}s, {steps} steps, {acc * 100:.3f}%, 
+              \t    avg path distance: {avg_dist:.4f}, 
+              \t    min: {min_dist}, q1: {q1_dist}, median: {med_dist}, q3: {q3_dist}, max: {max_dist})""")
 
     print()
 
