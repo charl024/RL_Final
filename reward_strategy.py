4241652e362c34cbbeba5fa5dc71a5de50bb7db5
charl024 <comaoeng2003@gmail.com>
2026-05-06 17:37:55 -0600

    decoupled visited from other strats

--- reward_strategy.py
+++ reward_strategy.py
@@ -6,8 +6,6 @@ OBSTACLE_REWARD = -100
 BOUNDARY_REWARD = -100
 DEFAULT_REWARD  = -1
 
-# higher means distance has more weight
-# lower means distance has less weight
 DISTANCE_SCALING_FACTOR = 0.5
 
 def reward_strategy_simple(event):
@@ -15,8 +13,6 @@ def reward_strategy_simple(event):
         return TARGET_REWARD
     if event in ["obstacle", "boundary"]:
         return OBSTACLE_REWARD
-    if event == "visited":
-        return VISITED_REWARD
     return DEFAULT_REWARD
 
 def reward_strategy_distance_based(event):
@@ -24,8 +20,6 @@ def reward_strategy_distance_based(event):
         return TARGET_REWARD
     if event in ["obstacle", "boundary"]:
         return OBSTACLE_REWARD
-    if event == "visited":
-        return VISITED_REWARD
 
     # manhattan distance
     return -event
@@ -35,11 +29,18 @@ def reward_strategy_distance_based_scaled(event):
         return TARGET_REWARD
     if event in ["obstacle", "boundary"]:
         return OBSTACLE_REWARD
-    if event == "visited":
-        return VISITED_REWARD
 
     # manhattan distance
     return DEFAULT_REWARD - DISTANCE_SCALING_FACTOR * event
+
+def reward_strategy_visited(event):
+    if event == "target":
+        return TARGET_REWARD
+    if event in ["obstacle", "boundary"]:
+        return OBSTACLE_REWARD
+    if event == "visited":
+        return VISITED_REWARD
+    return DEFAULT_REWARD
 
 def reward_strategy_harsh_visited(event):
     if event == "target":
@@ -47,5 +48,8 @@ def reward_strategy_harsh_visited(event):
     if event in ["obstacle", "boundary"]:
         return OBSTACLE_REWARD
     if event == "visited":
-        return -500
+        return VISITED_REWARD * 10
     return DEFAULT_REWARD
+
+reward_strategy_visited.uses_visited = True
+reward_strategy_harsh_visited.uses_visited = True
